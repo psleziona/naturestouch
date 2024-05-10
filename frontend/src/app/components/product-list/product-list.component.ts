d433f97f715df79763345756a5ebcdd0d21efb58
Michał <micmar007@utp.edu.pl>
2024-05-10 21:27:47 +0200

    product-list.component, styles

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -2,10 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from "../../_services/product.service";
 import { Product } from "../../_models/product.model";
 import { ProductTileComponent } from "../product-tile/product-tile.component";
-import { NgForOf, CommonModule} from "@angular/common";
+import { NgForOf, CommonModule } from "@angular/common";
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
 
 @Component({
   selector: 'app-product-list',
@@ -23,42 +22,52 @@ import { RouterModule } from '@angular/router';
 export class ProductListComponent implements OnInit {
   products!: Product[];
   sortOrder: string = '';
+  filteredProducts: Product[] = [];
+  categories: string[] = [];
+  selectedCategory = '';
 
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
-    this.productService.getProducts()
-      .subscribe(res => {
-        this.products = res;
-      });
+    this.loadProducts();
   }
 
-  sortProducts(): void {
-    switch (this.sortOrder) {
-      case 'name-asc':
-        this.products.sort((a, b) => a.name.localeCompare(b.name));
-        break;
-      case 'name-desc':
-        this.products.sort((a, b) => b.name.localeCompare(a.name));
-        break;
-      case 'price-asc':
-        this.products.sort((a, b) => a.price - b.price);
-        break;
-      case 'price-desc':
-        this.products.sort((a, b) => b.price - a.price);
-        break;
-    }
-  }
-  addToObserved(idProduct: number | undefined): void {
-    if (idProduct === undefined) {
-      console.error('Product ID is undefined');
-      return;
-    }
-    this.productService.addProductToObserved(idProduct).subscribe({
-      next: () => alert('Product added to observed list.'),
-      error: (err) => console.error('Failed to add product to observed', err)
+  loadProducts() {
+    this.productService.getProducts().subscribe(res => {
+      this.products = res;
+      this.filteredProducts = res;
+      this.extractCategories();
     });
   }
 
+  filterByCategory() {
+    if (!this.selectedCategory || this.selectedCategory === '') {
+      this.filteredProducts = [...this.products]; // Resets to the full list
+    } else {
+      this.filteredProducts = this.products.filter(p => p.category === this.selectedCategory);
+    }
+    this.sortProducts();
+  }
 
+  extractCategories() {
+    const categorySet = new Set(this.products.map(p => p.category));
+    this.categories = Array.from(categorySet);
+  }
+
+  sortProducts() {
+    this.filteredProducts = this.filteredProducts.sort((a, b) => {
+      switch (this.sortOrder) {
+        case 'name-asc':
+          return a.name.localeCompare(b.name);
+        case 'name-desc':
+          return b.name.localeCompare(a.name);
+        case 'price-asc':
+          return a.price - b.price;
+        case 'price-desc':
+          return b.price - a.price;
+        default:
+          return 0;
+      }
+    });
+  }
 }
